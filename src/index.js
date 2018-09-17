2a1ba85dffb6223fb1a3c759997efc607e134f54
Alexander Balan <fantom141141@gmail.com>
2018-09-18 02:52:47 +0300

    Implements advanced zeros

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    var multipliers = [];
+    var i = 2;
+    while (i <= base) {
+        if (base % i === 0) {
+            base = base / i;
+            multipliers.push(i);
+        } else {
+            i++;
+        }
+    }
+
+    divider = multipliers.sort((a, b) => b - a)[0];
+    function fact(zeroCount, val, fac) {
+        if (Math.round(val / fac) >= 1) {
+            zeroCount += ((val - (val % fac)) / fac);
+            return fact(zeroCount, val, fac * divider);
+        }
+        return zeroCount;
+    }
+
+    return fact(0, number, divider);
 }
